d31dae452353c32c89e3eebd65a24cb3fba33c0e
koteeswari <dskoteeswari@gmail.com>
2023-08-19 15:23:48 +0200

    roboshop mongodb installing

--- mongodb.sh
+++ mongodb.sh
@@ -1,13 +1,13 @@
 echo -e "\e[33mCopying  mongodb repo file\e[0m"
 
-cp mongodb.repo /etc/yum.repos.d/mongo.repo
+cp mongodb.repo /etc/yum.repos.d/mongo.repo &>>/tmp/roboshop.log
 
 echo -e "\e[33mInstalling mongodb\e[0m"
-yum install mongodb-org -y
+yum install mongodb-org -y &>>/tmp/roboshop.log
 
 echo -e "\e[33mEnable mongodb server\e[0m"
-systemctl enable mongodb
+systemctl enable mongodb &>>/tmp/roboshop.log
 
 #modify the config file
 echo -e "\e[33mStart mongodb server\e[0m"
-systemctl restart mongod
+systemctl restart mongod &>>/tmp/roboshop.log
